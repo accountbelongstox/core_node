e0f41e6c00c507b4dac93586d1ce691357af1612
DevOps User <devops@example.com>
2025-11-26 06:47:41 +0700

    winup.
    Former-commit-id: 9eb4c78047944af9cfd139c609ad3ed5d95a3353

--- poly_apps/nuxt_main/nuxt.config.ts
+++ poly_apps/nuxt_main/nuxt.config.ts
@@ -79,6 +79,14 @@ export default defineNuxtConfig({
                     rel: 'stylesheet',
                     href: 'https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700;800&display=swap',
                 },
+                {
+                    rel: 'stylesheet',
+                    href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap',
+                },
+                {
+                    rel: 'stylesheet',
+                    href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+                },
             ],
         },
     },
