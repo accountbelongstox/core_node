487ed80723445b1caeff79cf83e6ed89326ff918
DevOps User <devops@example.com>
2025-11-27 22:30:34 +0700

    local

--- scripts/pytools/special_software_env_manager/secret_read.py
+++ scripts/pytools/special_software_env_manager/secret_read.py
@@ -12,7 +12,7 @@ from pathlib import Path
 from typing import Optional
 
 
-PROJECT_ROOT = Path(__file__).resolve().parents[2]
+PROJECT_ROOT = Path(__file__).resolve().parents[3]
 RAW_SECRET_DIR = PROJECT_ROOT / ".secret_keys" / ".secret_ignore"
 
 
