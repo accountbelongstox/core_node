07cca29242eeea311c5f17d41e0f95fff66e409b
prop-dev <prop-dev@serve.com>
2025-12-06 23:09:51 +0700

    [AUTO] Pre-commit before user input - 2025-12-06 23:09:51
    Former-commit-id: 88b94e106835e783a51d5fc8aa233555ed1a689f

--- poly_apps/laravel_main/app/Apps/AppQyV1/AppQyV1Controllers/AppQyV1WordQurey/AppQyV1WordLookupController.php
+++ poly_apps/laravel_main/app/Apps/AppQyV1/AppQyV1Controllers/AppQyV1WordQurey/AppQyV1WordLookupController.php
@@ -3,7 +3,7 @@
 namespace App\Apps\AppQyV1\AppQyV1Controllers\AppQyV1WordQurey;
 
 use App\Http\Controllers\Controller;
-use App\Apps\AppQyV1\Utils\AppQyV1AITools\AppQyV1TTSService;
+use App\Services\EdgeTTS\EdgeTTSService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -13,7 +13,7 @@ class AppQyV1WordLookupController extends Controller
     
     public function __construct()
     {
-        $this->ttsService = new AppQyV1TTSService();
+        $this->ttsService = new EdgeTTSService();
     }
     
     public function lookup(Request $request)
