564ed1ccd11700b59e82af62556331c9df7a1b38
prop-dev <prop-dev@serve.com>
2026-09-22 19:01:18 +1000

    linux0.1

--- pycore/pylauncher/tray_codesync_cache.py
+++ pycore/pylauncher/tray_codesync_cache.py
@@ -13,7 +13,7 @@ This module keeps a THREAD_BUS-backed snapshot refreshed from a background threa
 from pycore.pyfoundations.serialized_worker import start_bus_task
 from pycore.pyfoundations.thread_bus.bus import THREAD_BUS
 from pycore.pyfoundations.thread_bus_constants import BusSignals
-from pycore.pybasecommon.color_print import ColorPrint
+from pycore.pyfoundations.pybasecommon.color_print import ColorPrint
 from pycore.pyutils.codesync.manager import get_code_sync_manager
 
 TRAY_CODESYNC_STATE_SIGNAL = BusSignals.TRAY_CODESYNC_STATE
